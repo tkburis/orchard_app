c09ac8320ace38149d1e1b4ffbee0f37758ea041
tkburis <tkburis@gmail.com>
2022-11-08 23:32:33 +0000

    Uncommented

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# from website import create_app
+from website import create_app
 
-# app = create_app()
+app = create_app()
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
